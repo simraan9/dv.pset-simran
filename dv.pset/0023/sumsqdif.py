7b67f6aaa39fabfb6e97e3cfc7f7df0c0378848b
Simran Singh <simran.singh9@yahoo.com>
2018-05-26 00:00:06 +0530

    sumsq

---
+++ dv.pset/0023/sumsqdif.py
@@ -0,0 +1,20 @@
+def sqSum():
+    i=1
+    while i<=10:
+        x=i**2
+        a=0+x
+        i=i+1
+    print (a)
+sqSum()
+
+def sumSq():
+    i=1
+    while i<=10:
+        x=i
+        b=0+i
+        i=i+1
+    return b
+    print (b**2)
+sumSq()
+
+print (a+b)
