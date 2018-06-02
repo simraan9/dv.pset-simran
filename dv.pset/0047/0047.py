e7077cfff14137966a6f673aa63cad3e2ac4bad9
Simran Singh <simran.singh9@yahoo.com>
2018-06-02 23:32:23 +0530

    0047
    special pythagorean triplet 1pt

---
+++ dv.pset/0047/0047.py
@@ -0,0 +1,10 @@
+def pythTriplet():
+    for a in range(1,1000):
+        for b in range(a,1000):
+            c = 1000-a-b
+            if c>0:
+                if c*c==a*a+b*b:
+                    print ("Pythagorean Triplet: ", a,b,c)
+                    print ("abc=",a*b*c)
+                    break
+pythTriplet()
