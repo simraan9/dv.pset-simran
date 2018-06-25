431d0f0f3c929d22f7764aa6a1237b28e9b2623b
Simran Singh <simran.singh9@yahoo.com>
2018-06-25 10:02:53 +0530

    0058-Next Date
    fixed July,August 31 day mistake

--- dv.pset/0058-Next Date/nextdate.py
+++ dv.pset/0058-Next Date/nextdate.py
@@ -4,11 +4,11 @@ yy=int(input("Enter year (yyyy):"))
 
 def nextDate(dd,mm,yy):
     #Months with 31 days
-    if dd==31 and (mm==1 or mm==3 or mm==5 or mm==7 or mm==9 or mm==11):
+    if dd==31 and (mm==1 or mm==3 or mm==5 or mm==7 or mm==8 or mm==10):
         dd=1
         mm=mm+1
     #Months with 30 days
-    elif dd==30 and (mm==4 or mm==6 or mm==8 or mm==10):
+    elif dd==30 and (mm==4 or mm==6 or mm==9 or mm==11):
         dd=1
         mm=mm+1
     #Not 31st as date months
@@ -43,4 +43,4 @@ def nextDate(dd,mm,yy):
     else:
         dd=dd+1
     return (dd,mm,yy)
-print(nextDate(dd,mm,yy))
+print('Next Date:',nextDate(dd,mm,yy))
